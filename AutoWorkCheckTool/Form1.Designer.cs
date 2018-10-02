ec565c69097575d9f945a93f04ba70fe05cc903f
ChoiShin <sin8392@gmail.com>
2018-10-03 02:20:39 +0900

    First Commit

---
+++ AutoWorkCheckTool/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace AutoWorkCheckTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WebViewBox = new System.Windows.Forms.GroupBox();
+            this.WebBrowserView = new System.Windows.Forms.WebBrowser();
+            this.ShutdownButton = new System.Windows.Forms.Button();
+            this.AccountTextBox = new System.Windows.Forms.TextBox();
+            this.WebViewBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // WebViewBox
+            // 
+            this.WebViewBox.Controls.Add(this.WebBrowserView);
+            this.WebViewBox.Location = new System.Drawing.Point(12, 78);
+            this.WebViewBox.Name = "WebViewBox";
+            this.WebViewBox.Size = new System.Drawing.Size(425, 449);
+            this.WebViewBox.TabIndex = 0;
+            this.WebViewBox.TabStop = false;
+            this.WebViewBox.Text = "WebViewBox";
+            // 
+            // WebBrowserView
+            // 
+            this.WebBrowserView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WebBrowserView.Location = new System.Drawing.Point(3, 17);
+            this.WebBrowserView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.WebBrowserView.Name = "WebBrowserView";
+            this.WebBrowserView.Size = new System.Drawing.Size(419, 429);
+            this.WebBrowserView.TabIndex = 0;
+            this.WebBrowserView.Url = new System.Uri("http://www.naver.com", System.UriKind.Absolute);
+            // 
+            // ShutdownButton
+            // 
+            this.ShutdownButton.Location = new System.Drawing.Point(362, 12);
+            this.ShutdownButton.Name = "ShutdownButton";
+            this.ShutdownButton.Size = new System.Drawing.Size(75, 23);
+            this.ShutdownButton.TabIndex = 3;
+            this.ShutdownButton.Text = "시스템종료";
+            this.ShutdownButton.UseVisualStyleBackColor = true;
+            this.ShutdownButton.Click += new System.EventHandler(this.ShutdownButton_Click);
+            // 
+            // AccountTextBox
+            // 
+            this.AccountTextBox.Location = new System.Drawing.Point(15, 12);
+            this.AccountTextBox.Name = "AccountTextBox";
+            this.AccountTextBox.Size = new System.Drawing.Size(212, 21);
+            this.AccountTextBox.TabIndex = 4;
+            this.AccountTextBox.Text = "sin8392/choi8392";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 539);
+            this.Controls.Add(this.AccountTextBox);
+            this.Controls.Add(this.ShutdownButton);
+            this.Controls.Add(this.WebViewBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WebViewBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox WebViewBox;
+        private System.Windows.Forms.WebBrowser WebBrowserView;
+        private System.Windows.Forms.Button ShutdownButton;
+        private System.Windows.Forms.TextBox AccountTextBox;
+    }
+}
+
---
+++ AutoWorkCheckTool/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using WindowsInput.Native;
+using WindowsInput;
+
+namespace AutoWorkCheckTool
+{
+    public partial class Form1 : Form
+    {
+        //This is a replacement for Cursor.Position in WinForms
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        public const int MOUSEEVENTF_LEFTUP = 0x04;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            //1. Wait First Web Page Open
+            WaitWebPageLoadingEnd();
+
+            //2. Ask Continue Process
+            DialogResult messageBoxResult = MessageBox.Show("정말로 출/퇴근 처리를 하시겠습니까?", "경고", MessageBoxButtons.YesNoCancel);
+            if (messageBoxResult == DialogResult.Yes)
+            {
+                Thread.Sleep(50);
+
+                //3. Login Web Page
+                InputSimulator sim = new InputSimulator();
+                string[] getAccount = AccountTextBox.Text.Split(new string[]{"/", "\n"}, StringSplitOptions.None);
+
+                //3-1. Input ID & Password
+                int count = 0;
+                foreach (string keyword in getAccount)
+                {
+                    Thread.Sleep(50);
+                    LeftMouseClick(this.Location.X + 350, this.Location.Y + 420 + (count * 50));
+
+                    char[] words = keyword.ToCharArray();
+
+                    foreach (char word in words)
+                    {
+                        sim.Keyboard.TextEntry(word).Sleep(1);
+                    }
+                    count += 1;
+                }
+                //3-2. Click Login Button
+                Thread.Sleep(50);
+                LeftMouseClick(this.Location.X + 50, this.Location.Y + 420);
+                WaitWebPageLoadingEnd();
+
+                //4. Move Web Page To Click  
+                Thread.Sleep(50);
+                LeftMouseClick(this.Location.X + 150, this.Location.Y + 420);
+                WaitWebPageLoadingEnd();
+
+                //5. Click Work Check Button
+                Thread.Sleep(50);
+                LeftMouseClick(this.Location.X + 200, this.Location.Y + 420);
+                WaitWebPageLoadingEnd();
+            }
+            else
+            {
+                //End
+            }
+
+            /*
+            WebBrowserView.Navigate(new Uri("http://www.naver.com"));
+            while (WebBrowserView.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            */
+        }
+
+        private void WaitWebPageLoadingEnd()
+        {
+            while (WebBrowserView.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+        }
+
+        private void ClickButton_Click(object sender, EventArgs e)
+        {
+            //LeftMouseClick(350, 420);
+        }
+
+        //This simulates a left mouse click
+        public static void LeftMouseClick(int xpos, int ypos)
+        {
+            SetCursorPos(xpos, ypos);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+
+        private void ShutdownButton_Click(object sender, EventArgs e)
+        {
+            //Turn Off Program & Windows System
+        }
+    }
+}
